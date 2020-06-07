ce3a3bdcca69c595a3f0034cc5c94eab646d085f
sten031 <stenstenktensten@gmail.com>
2020-06-07 16:13:18 +0900

    finish Chapter01-04

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+/*「Chapter04.java」に以下のプログラムを作成してください。
+　・自分の名前を「String」型の変数に代入して出力
+　・好きな言葉を同じ変数に再代入して出力
+　・ラッキーナンバーとして「int」型で宣言した定数を出力*/
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "あああ　いいい";
+		System.out.println(str);
+		str = "aaa iii";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
