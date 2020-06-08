07ddfc4576c9ae3f9e35d7500c1fcb9e11fcb9f1
sten031 <stenstenktensten@gmail.com>
2020-06-08 16:17:12 +0900

    finish chapter05

---
+++ src/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        System.out.println(l);
+        double d = 3.33;
+        System.out.println(d);
+        boolean b = true;
+        System.out.println(b);
+
+        Integer i = 123;
+        System.out.println(i);
+        Float f = 1.4133f;
+        System.out.println(f);
+        Character c = 'あ';
+        System.out.println(c);
+
+        int[] array = { 1, 2, 3, 4, 5 };
+        System.out.println(array[3]);
+    }
+}
